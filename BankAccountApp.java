f950587de2ccc6f0a47a605413f090b6679f1220
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2024-06-19 17:58:19 +0000

    Create BankAccountApp.java

---
+++ BankAccountApp.java
@@ -0,0 +1,16 @@
+package bankaccountapp;
+
+public class BankAccountApp {
+
+	public static void main(String[] args) {
+		
+		Checking chkacc1 = new Checking("shivi bhargv");
+		
+		Savings savacc1 = new Savings("Rich john");
+	
+		
+		// read a csv file then create new accounts based on that data
+
+	}
+
+}
