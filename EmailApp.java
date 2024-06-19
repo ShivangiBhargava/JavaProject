55f2f32023b2136526992f4ccc6d2303c6577942
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2024-06-19 08:00:17 +0000

    Final_Update EmailApp.java

--- EmailApp.java
+++ EmailApp.java
@@ -1,13 +1,19 @@
+//for creating objects using this class
+
 package emailapp;
 
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Email em1 = new Email("John","Smith");
-		em1.setAlternateEmail("js@gmail.com");
-		System.out.println(em1.getAlternateEmail());
-
+// set the constructor
+// ClassName objName = new ClassName(values);
+		Email em1 = new Email("John", "Smith");
+		
+		//em1.setAlternateEmail("js@gmail.com");
+		//System.out.println(em1.getAlternateEmail());
+		// if returning string (return) then use sysout
+		System.out.println(em1.showInfo());
+		
 	}
 
 }
