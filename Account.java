319ef7c8d5fa83731e46f263e5d3e89347153a16
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2024-06-19 17:57:32 +0000

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,15 @@
+// account is parent or super class  for sharing common properties of savings and checking
+package bankaccountapp;
+
+public abstract class Account implements IBaseRate {
+	// list common properties for savings and checking accounts
+	
+	// constructor to set base properties and initialize the account
+	public Account(String name) {
+		System.out.println("NAME: "+name);
+		System.out.print("NEW ACCOUNT ");
+	}
+	
+	// list common methods
+
+}
