638480dff77010fdd262b006676b9409031bf189
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2024-06-19 18:00:23 +0000

    Create Savings.java

---
+++ Savings.java
@@ -0,0 +1,14 @@
+package bankaccountapp;
+
+public class Savings extends Account {
+	// list properties specific to the Savings account
+	
+	// constructor to initialize settings for the Savings properties
+	public Savings(String name) {
+		super(name);
+		System.out.println("NEW SAVINGS ACCOUNT");
+	}
+	
+	// list any methods specific to savings account
+
+}
