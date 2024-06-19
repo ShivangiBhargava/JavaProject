328224d1d62a0c3ed915fd967a110fcf82e4f90e
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2024-06-19 17:59:01 +0000

    Create Checking.java

---
+++ Checking.java
@@ -0,0 +1,14 @@
+package bankaccountapp;
+
+public class Checking extends Account {
+	// list properties specific to a Checking account
+	
+	// constructor to initialize checking account properties
+	public Checking(String name) {
+		super(name);
+		System.out.println("NEW CHECKING ACCOUNT");
+	}
+	
+	// list any methods specific to checking account
+
+}
