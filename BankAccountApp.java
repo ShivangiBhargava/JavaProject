3eab7e8f2e57ce636cb19487e4fccc519e004aed
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2024-06-20 18:32:38 +0000

    Update BankAccountApp.java

--- BankAccountApp.java
+++ BankAccountApp.java
@@ -4,9 +4,18 @@ public class BankAccountApp {
 
 	public static void main(String[] args) {
 		
-		Checking chkacc1 = new Checking("shivi bhargv");
+		Checking chkacc1 = new Checking("shivi bhargv","123456789",1500);
 		
-		Savings savacc1 = new Savings("Rich john");
+		Savings savacc1 = new Savings("Rich john","987654321",2000);
+		
+		savacc1.showInfo();
+		System.out.println("************************");
+		chkacc1.showInfo();
+	
+		//savacc1.deposit(500);
+		//savacc1.withdraw(500);
+		//savacc1.transfer("Brokerage", 100);
+		//savacc1.compound();
 	
 		
 		// read a csv file then create new accounts based on that data
